05d0ca1c4b761d8339b4cf1bbe9927533f1e0624
Rainbow1990 <Rainbow10222@gmail.com>
2022-12-14 18:12:39 +0300

    Debugging

---
+++ Catch Arguments Passed in the Wrong Order When Calling a Function.js
@@ -0,0 +1,8 @@
+function raiseToPower(base, exponent) {
+    return Math.pow(base, exponent);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
